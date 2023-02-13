289d554f484df7efd40dc80f85d43b7d59ef1f28
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-02-13 16:27:57 +0300

    Project created

---
+++ trafficLightOnSwiftUI/trafficLightOnSwiftUI/trafficLightOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightOnSwiftUIApp.swift
+//  trafficLightOnSwiftUI
+//
+//  Created by dzmitry on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
